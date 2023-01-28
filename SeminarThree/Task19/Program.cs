a48413a36711854443a56b5c5c5c647b786e4fc6
«Maxim <realmazepa@gmail.com>
2023-01-28 21:17:01 +0300

    added task19

---
+++ SeminarThree/Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int rev = 0;
+int chek = num;
+
+while (num > 0)
+{
+    int rem = num % 10;
+    rev = (rev * 10) + rem;
+    num = num / 10;
+}
+Console.WriteLine($"Новое число --> {rev}");
+
+if (chek == rev)
+    {
+        System.Console.WriteLine($"Ваше число ЯВЛЯЕТСЯ палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine($"Ваше число НЕ является палиндромом");
+    }
+
---
+++ SeminarThree/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.Write("Введите цифру или число: ");
+double N = Convert.ToInt32(System.Console.ReadLine());
+double num = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    num = Math.Pow(i,3);
+    System.Console.WriteLine($"{i} --> {num}");
+}

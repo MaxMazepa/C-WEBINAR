0ac0975e89074b54eefad10a23c0ddc96aecdb92
«Maxim <realmazepa@gmail.com>
2023-01-31 17:19:02 +0300

    Task27Com

--- SeminarFour/Task27/Program.cs
+++ SeminarFour/Task27/Program.cs
@@ -3,15 +3,28 @@
 // 82 -> 10
 // 9012 -> 12
 
-int num = new Random().Next(111, 10000);
-int sum = 0;
-System.Console.WriteLine($"Исходное число --> {num}");
-
-while (num/10 > 0)
+Console.WriteLine($"Сумма цифр числа --> {GetSum(new Random().Next(100,10000))}");
+static int GetSum(int num)
 {
-    sum = sum + (num%10);
-    num= num/10;
+    System.Console.WriteLine($"Исходное число --> {num}");
+    int sum = 0;
+    while (num / 10 > 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    sum = sum + (num % 10); // <---((Не получилось поставить верное условие в while, чтобы избежать этого действия))
+    return sum;
 }
-sum = sum + (num%10);
-System.Console.WriteLine($"Сумма цифр числа -- > {sum}");
 
+// int num = new Random().Next(111, 10000);
+// int sum = 0;
+// System.Console.WriteLine($"Исходное число --> {num}");
+
+// while (num / 10 > 0)                                             Первый вариант решения
+// {
+//     sum = sum + (num % 10);
+//     num = num / 10;
+// }
+// sum = sum + (num % 10);
+// System.Console.WriteLine($"Сумма цифр числа -- > {sum}");
